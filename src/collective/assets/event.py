096550f5fd02e240c3bf4bb20e7eab0acf829070
Tom Gross <itconsense@gmail.com>
2013-10-10 13:26:43 +0200

    fixed event for non site traversing

--- src/collective/assets/event.py
+++ src/collective/assets/event.py
@@ -9,6 +9,9 @@ def generate_assets(event):
     env = zope.component.getUtility(IWebAssetsEnvironment)
     if not len(env):
         site = zope.component.hooks.getSite()
+        if site is None:
+            # No plone Site
+            return
         generateview = zope.component.queryMultiAdapter(
                 (site, event.request), name="generate-assets")
         LOG.warn('No assets found. Generating them.')
